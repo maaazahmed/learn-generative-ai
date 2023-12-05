1fd7ca68f299cf944519e4e6490de57478b3b5ec
maaazahmed <maazahmeed2k16@gmail.com>
2023-12-06 02:37:21 +0500

    Function call complete code

---
+++ 05_function_calling/type.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+messages:list[dict[str, str]] = [{"role":"user", "content":"main_request"}]
+
+
+
+tools:list[dict[str, Union[str, dict]]]  = [
+        {
+            "type": "function",
+            "function": {
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "The city and state, e.g. San Francisco, CA",
+                        },
+                        "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                    },
+                    "required": ["location"],
+                },
+            },
+        }
+    ]
+    
