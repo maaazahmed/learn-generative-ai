685aa287107777672d1f27a46cd4fb708210f897
maaazahmed <maazahmeed2k16@gmail.com>
2024-01-02 21:24:30 +0500

    updated

--- 05_ui_streamlit/12_chat_app_project/chat_app_project.py
+++ 05_ui_streamlit/12_chat_app_project/chat_app_project.py
@@ -1,20 +1,21 @@
 import streamlit as st
-
+import random
+import time
+from openai import OpenAI
 
 st.title("Echo Bot")
 
+# st.info("API_KEY: {}".format(st.secrets["OPENAI_API_KEY"]))
+
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo-1106"
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {
-            "role":"user",
-            "content":"Hi, how are you"
-        },
-        {
-            "role":"assistant",
-            "content":"I am good"
-        },
-    ]
+    st.session_state.messages = []
 
 
 for message in st.session_state.messages:
@@ -26,63 +27,44 @@ for message in st.session_state.messages:
 prompt = st.chat_input("Say something")
 
 if prompt:
-    
+    st.session_state.messages.append({"role":"user", "content":prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-    st.session_state.messages.append({"role":"user", "content":prompt})
-
-    response = f"Echo: {prompt}"
 
     with st.chat_message("assistant"):
-        st.markdown(response)
-    st.session_state.messages.append({"role":"assistant", "content":response}) 
+        message_placeholder = st.empty()
+        full_response:str = ""
 
-
-
-
-
-         
-
-
-
-
-
-
-
-# st.title("Echo Bot")
-
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
-
-
-
-
-
-
-
-
-
-
-
-
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])    
-
-
-
-
-
-# if prompt:=st.chat_input("What is up?"):
+        chat_response = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[{"role":m["role"],"content":m["content"]} for m in st.session_state["messages"]],
+            stream=True
+        )
         
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
-#     st.session_state.messages.append({"role":"user", "content":prompt})  
-        
-#     with st.chat_message("assistant"):
-#         st.markdown(f"Echo: {prompt}")
-#     st.session_state.messages.append({"role": "assistant", "content": response})        
+
+    for response in chat_response:
+        if(response.choices[0].delta.content):
+            full_response += response.choices[0].delta.content
+
+        message_placeholder.markdown(full_response)
+    message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role":"assistant", "content":full_response})
     
-         
 
+
+
+
+
+
+
+
+# for response in chat_response:
+#     full_response += (response.choices[0].delta.content or "")
+#     st.markdown(full_response)
+
+    #     for response in chat_response:
+    #         full_response += (response.choices[0].delta.content or "")
+    #         message_placeholder.markdown(full_response  + " ")
+    #     message_placeholder.markdown(full_response)
+    # st.session_state.messages.append({"role":"assistant", "content":full_response})    
 
